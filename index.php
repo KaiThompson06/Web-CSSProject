454b56aa56530682a3d6e2df4110a50349794217
Kai Thompson <kaihrthompson@gmail.com>
2025-03-29 18:14:39 -0400

    index scaffold

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- all meta data -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="basic index page">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="author" content="Kai Thompson">
+    <title>Home</title>
+    <!-- link to the css file -->
+    <link rel="stylesheet" href="./css/styles.css">
+    <!-- link to the js file -->
+    <script src="./js/scripts.js" defer></script>
+    <!-- link to font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- add the header -->
+<?php
+include './templates/header.php';
+?>
+<main>
+    <?php
+
+    ?>
+</main>
+<!-- add the footer -->
+<?php
+include './templates/footer.php';
+?>
+</body>
+</html>
